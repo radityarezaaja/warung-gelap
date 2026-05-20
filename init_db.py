253ee8523a556fbcefd3a2af37cf60dd59ee6ff0
radityarezaaja <dwireza353@gmail.com>
2026-05-20 21:25:25 +0700

    Fix URL fetch agar mengarah ke server Vercel

--- init_db.py
+++ init_db.py
@@ -4,7 +4,7 @@ import pymysql
 db_config = {
     "host": "mysql-f0fb579-warunggelap.a.aivencloud.com",
     "user": "avnadmin",
-    "password": "AVNS_a5l9XjufF6hO563UcRe", # <-- GANTI PAKE PASSWORD AIVEN LO!
+    "password": "AVNS_zBSkHwPh5HwgJipez9a", # <-- GANTI PAKE PASSWORD AIVEN LO!
     "database": "defaultdb",
     "port": 13745,
     "ssl": {'ssl': {}}
